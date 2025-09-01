77c9f684586e49fb4948db87aa684ade919602b6
Opanasenko-Mykhailo <124090722+Opanasenko-Mykhailo@users.noreply.github.com>
2025-09-01 13:53:54 +0300

    init commit

---
+++ src/main/java/com/gcm/GymCrmMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.gcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCrmMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymCrmMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gcm/GymCrmMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymCrmMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
