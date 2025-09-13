846c40729cbe8bf8f5a75a2247d3f4f220935e6f
”Opanasenko-Mykhailo <opanasenko.misha@gmail.com>
2025-09-13 13:08:27 +0300

    GCA-181 Migrate workload updates to ActiveMQ - JMS Connection Setup

---
+++ workload-service/src/main/java/com/gcm/config/JmsConfig.java
@@ -0,0 +1,37 @@
+package com.gcm.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+@EnableJms
+public class JmsConfig {
+
+    @Bean
+    public MappingJackson2MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        converter.setObjectMapper(objectMapper);
+
+        return converter;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(CachingConnectionFactory connectionFactory,
+                                   MappingJackson2MessageConverter converter,
+            @Value("${jms.request-timeout-ms:5000}") long receiveTimeout) {
+        JmsTemplate template = new JmsTemplate(connectionFactory);
+        template.setMessageConverter(converter);
+        template.setReceiveTimeout(receiveTimeout);
+
+        return template;
+    }
+}
