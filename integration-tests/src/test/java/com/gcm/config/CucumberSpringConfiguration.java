d6e625db8f2cb49140bd03f355b081d36b2d6230
Opanasenko-Mykhailo <124090722+Opanasenko-Mykhailo@users.noreply.github.com>
2025-10-06 15:35:17 +0300

    GCA-209 Implement integration tests - Integrate Docker for test environment

--- integration-tests/src/test/java/com/gcm/config/CucumberSpringConfiguration.java
+++ integration-tests/src/test/java/com/gcm/config/CucumberSpringConfiguration.java
@@ -3,34 +3,11 @@ package com.gcm.config;
 import com.gcm.CucumberIntegrationApplication;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.activemq.ActiveMQContainer;
-import org.testcontainers.utility.DockerImageName;
-
-import java.util.List;
+import org.springframework.test.context.ContextConfiguration;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = CucumberIntegrationApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = TestContainersInitializer.class)
 public class CucumberSpringConfiguration {
-
-    private static final int ACTIVEMQ_PORT = 61616;
-
-    private static final ActiveMQContainer ACTIVE_MQ_CONTAINER =
-            new ActiveMQContainer(
-                    DockerImageName.parse("rmohr/activemq:latest")
-                            .asCompatibleSubstituteFor("apache/activemq-classic"))
-                    .withExposedPorts(ACTIVEMQ_PORT)
-                    .withReuse(true);
-
-    static {
-        ACTIVE_MQ_CONTAINER.setPortBindings(List.of(ACTIVEMQ_PORT + ":" + ACTIVEMQ_PORT));
-        ACTIVE_MQ_CONTAINER.start();
-    }
-
-    @DynamicPropertySource
-    static void registerActiveMqProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.activemq.broker-url", () -> "tcp://localhost:" + ACTIVEMQ_PORT);
-    }
 }
---
+++ integration-tests/src/test/java/com/gcm/config/TestContainersInitializer.java
@@ -0,0 +1,30 @@
+package com.gcm.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.io.File;
+import java.time.Duration;
+
+public class TestContainersInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
+        DockerComposeContainer<?> environment = new DockerComposeContainer<>(new File("docker-compose.yml"))
+                .withOptions("--compatibility")
+                .withExposedService("activemq", 61616)
+                .withExposedService("mongodb", 27017)
+                .withExposedService("gca-core-service", 8081,
+                        Wait.forHttp("/gym-crm-core/api/v1/trainees/nobody")
+                                .withMethod("GET")
+                                .forStatusCode(401)
+                                .withStartupTimeout(Duration.ofSeconds(120)))
+                .withExposedService("workload-service", 8082)
+                .withLocalCompose(true);
+
+        environment.start();
+    }
+}
